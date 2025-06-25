22e93ca179a9ef8448a3fff5ce0c256b06a7c2e0
VaibhavRajDwivedi <vaibhavrajdwivedi65@gmail.com>
2025-06-26 03:20:19 +0530

    few changes

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -34,7 +34,7 @@ function AdminDashboard() {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("role");
-    window.location.href = "/login";
+    navigate('/');
   };
 
   return (
